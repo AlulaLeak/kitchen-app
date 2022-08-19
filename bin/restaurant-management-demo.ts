e9fda1ec665f59328be25d1633d57048ce67bcbd
AlulaLeak <alula.leakemariam94@gmail.com>
2022-08-19 15:37:37 -0400

    added order table

--- bin/restaurant-management-demo.ts
+++ bin/restaurant-management-demo.ts
@@ -2,6 +2,8 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { ExistingResources } from "../lib/existing-resources";
+import { SharedStack } from "../lib/shared-stack";
 
 const app = new cdk.App();
 const existingResources = new ExistingResources(app, "ExistingResources");
+const sharedStack = new SharedStack(app, "Sharedstack");
--- lib/shared-stack.ts
+++ lib/shared-stack.ts
@@ -1,12 +1,45 @@
-import { aws_s3 as s3 } from "aws-cdk-lib";
+import { aws_dynamodb as ddb, aws_lambda as lambda } from "aws-cdk-lib";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-export class ExistingResources extends cdk.Stack {
-  public readonly archiveBucket: s3.Bucket;
+export class SharedStack extends cdk.Stack {
+  public readonly database: ddb.Table;
+  public readonly layer: lambda.LayerVersion;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id);
-    this.archiveBucket = new s3.Bucket(this, "existing-order-archive");
+    this.database = new ddb.Table(this, "order-table", {
+      partitionKey: {
+        name: "id",
+        type: ddb.AttributeType.STRING,
+      },
+      billingMode: ddb.BillingMode.PROVISIONED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    // Add auto-scaling
+    const readScaling = this.database.autoScaleReadCapacity({
+      minCapacity: 1,
+      maxCapacity: 50,
+    });
+    readScaling.scaleOnUtilization({
+      targetUtilizationPercent: 50,
+    });
+    // Add GSI
+    this.database.addGlobalSecondaryIndex({
+      partitionKey: {
+        name: "gsi1pk",
+        type: ddb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "gsi1pk",
+        type: ddb.AttributeType.STRING,
+      },
+      indexName: "gsi1pk-gsi1pk-index",
+    });
+    // Add a Lambda layer for sharing database functions
+    this.layer = new lambda.LayerVersion(this, "db-functions-layer", {
+      code: lambda.Code.fromAsset("lambda"),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_14_X],
+    });
   }
 }
